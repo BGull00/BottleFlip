934cad668b5f31c2dca89d9f82d5349642b81383
trogers19 <tnrogers019@gmail.com>
2022-12-11 16:29:58 -0500

    Add Python RL files

---
+++ DQN_agent.py
@@ -0,0 +1,81 @@
+
+
+# Import Required Packages
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+
+import numpy as np
+import random
+from collections import namedtuple, deque
+
+from model import QNetwork
+from replay_memory import ReplayBuffer
+
+# Use CPU or GPU computation
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+class Agent():
+
+     # Initialize agent with parameters for DQN
+    def __init__(self, state_size, action_size, dqn_type='DQN', replay_memory_size=1e5, batch_size=64, gamma=0.99, learning_rate=1e-3, target_tau=2e-3, update_rate=4, seed=0):
+        self.dqn_type = dqn_type
+        self.state_size = state_size
+        self.action_size = action_size
+        self.buffer_size = int(replay_memory_size)
+        self.batch_size = batch_size
+        self.gamma = gamma
+        self.learn_rate = learning_rate
+        self.tau = target_tau
+        self.update_rate = update_rate
+        self.seed = random.seed(seed)
+
+        self.network = QNetwork(state_size, action_size, seed).to(device)
+        self.target_network = QNetwork(state_size, action_size, seed).to(device)
+        self.optimizer = optim.Adam(self.network.parameters(), lr=self.learn_rate)
+
+        # Replay memory
+        self.memory = ReplayBuffer(action_size, self.buffer_size, self.batch_size, seed)
+
+        # Initialize time step (for updating every UPDATE_EVERY steps)
+        self.t_step = 0
+
+    def step(self, state, action, reward, next_state, done):
+        # Save experience in replay memory
+        self.memory.add(state, action, reward, next_state, done)
+        
+        # Learn every UPDATE_EVERY time steps.
+        self.t_step = (self.t_step + 1) % self.update_rate
+        if self.t_step == 0:
+            # If enough samples are available in memory, get random subset and learn
+            if len(self.memory) > self.batch_size:
+                experiences = self.memory.sample()
+                self.learn(experiences, self.gamma)
+
+
+    def act(self, state, eps=0.0):
+        """Returns actions for given state as per current policy.
+        
+        Params
+        ======
+            state (array_like): current state
+            eps (float): epsilon, for epsilon-greedy action selection
+        """
+        state = torch.from_numpy(state).float().unsqueeze(0).to(device)
+        self.network.eval()
+        with torch.no_grad():
+            action_values = self.network(state)
+        self.network.train()
+
+        # Epsilon-greedy action selection
+        if random.random() > eps:
+            return np.argmax(action_values.cpu().data.numpy())
+        else:
+            return random.choice(np.arange(self.action_size))
+
+
+
+
+
+
---
+++ DQN_with_KerasRL.py
@@ -0,0 +1,78 @@
+
+"""
+ECE 414 Final Project
+
+Based heavily on example from Keras-RL
+https://github.com/keras-rl/keras-rl/blob/master/examples/duel_dqn_cartpole.py
+"""
+
+import numpy as np
+
+from mlagents_envs.environment import UnityEnvironment
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+from keras.optimizers import Adam
+from mlagents_envs.environment import UnityEnvironment
+
+from rl.agents.dqn import DQNAgent
+from rl.policy import BoltzmannQPolicy
+from rl.memory import SequentialMemory
+
+ENV_NAME = "BottleFlipEnv"
+
+# Load the environment using ML-Agents Python Low Level API
+#
+env = UnityEnvironment(file_name="BottleFlipEnv")
+
+# Reset the environment to start with a blank slate
+env.reset()
+
+# Get environment details
+behavior_name = env.get_behavior_names()[0]
+behavior_spec = env.get_behavior_spec(behavior_name)
+
+n_actions = behavior_spec.action_size
+state_dims = np.sum(behavior_spec.observation_shapes)  # list[tuple(52,), tuple(2,)] => 54 total obs
+
+if behavior_spec.is_action_continuous():
+    print("Action space is CONTINUOUS")
+else:
+    print("Action space is DISCRETE")
+    print(behavior_spec.discrete_action_branches)
+
+
+np.random.seed(123)
+env.seed(123)
+
+# Build simple model
+model = Sequential()
+model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
+model.add(Dense(16))
+model.add(Activation('relu'))
+model.add(Dense(16))
+model.add(Activation('relu'))
+model.add(Dense(16))
+model.add(Activation('relu'))
+model.add(Dense(n_actions, activation='linear'))
+print(model.summary())
+
+# Configure and compile an agent
+memory = SequentialMemory(limit=50000, window_length=1)
+policy = BoltzmannQPolicy()
+# enable the dueling network
+# you can specify the dueling_type to one of {'avg','max','naive'}
+dqn = DQNAgent(model=model, nb_actions=n_actions, memory=memory, nb_steps_warmup=10,
+               enable_dueling_network=True, dueling_type='avg', target_model_update=1e-2, policy=policy)
+dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+
+# Okay, now it's time to learn something! We visualize the training here for show, but this
+# slows down training quite a lot. You can always safely abort the training prematurely using
+# Ctrl + C.
+dqn.fit(env, nb_steps=50000, visualize=True, verbose=2)
+
+# After training is done, we save the final weights.
+dqn.save_weights('duel_dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
+
+# Finally, evaluate our algorithm for 5 episodes.
+dqn.test(env, nb_episodes=5, visualize=True)
