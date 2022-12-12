0e7192d06c0388a380809647a54ad20e8aa0377b
trogers19 <tnrogers019@gmail.com>
2022-12-12 00:51:11 -0500

    Update DQN code, add initial NAF code

--- DQN_with_KerasRL.py
+++ DQN_with_KerasRL.py
@@ -7,6 +7,7 @@ https://github.com/keras-rl/keras-rl/blob/master/examples/duel_dqn_cartpole.py
 """
 
 import numpy as np
+import rl.callbacks
 
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten
@@ -25,7 +26,7 @@ from rl.memory import SequentialMemory
 
 
 channel = EngineConfigurationChannel()
-channel.set_configuration_parameters(time_scale=20.0)
+channel.set_configuration_parameters(time_scale=50.0)
 
 # Load the environment using ML-Agents Python Low Level API
 unity_env = UnityEnvironment(side_channels=[channel])
@@ -50,7 +51,7 @@ nb_actions = env.action_size
 
 # Build a simple model
 model = Sequential()
-model.add(Flatten(input_shape=(1,) + (1,4)))
+model.add(Flatten(input_shape=(1,) + (1, 4)))
 model.add(Dense(16))
 model.add(Activation('relu'))
 model.add(Dense(16))
@@ -74,10 +75,14 @@ dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 train_log_dir = "logs/fit/" + current_time
 test_log_dir = "logs/test/" + current_time
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=train_log_dir, histogram_freq=1)
+train_tb = tf.keras.callbacks.TensorBoard(log_dir=train_log_dir, histogram_freq=1)
+test_tb = tf.keras.callbacks.TensorBoard(log_dir=test_log_dir, histogram_freq=1)
+
+cb_filepath = "DQN_results/" + current_time
+cb = rl.callbacks.FileLogger(filepath=cb_filepath)
 
 # Train with DQN
-dqn.fit(env, nb_steps=50000000000000, visualize=True, verbose=2)
+dqn.fit(env, nb_steps=15000000, visualize=True, verbose=2, callbacks=[cb])
 
 # Save the final weights
 dqn.save_weights('dqn_{}_weights.h5f'.format("BottleFlipEnv"), overwrite=True)
